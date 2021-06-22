673141266fbd6b09f394f3af2708643a55eb6dcc
ADXvenom <72479529+ADXvenom@users.noreply.github.com>
2021-06-22 18:19:13 -0500

    Add files via upload

---
+++ sketch1.js
@@ -0,0 +1,28 @@
+var num = 2;
+var number = "37";
+var g
+
+function preload(){
+
+}
+
+function setup(){
+
+    console.log(num)
+    console.log(number)
+    console.log(parseInt(number))
+    console.log(g)
+    for(i = 5; i > 0; i--){
+        console.log(i);
+    }
+    i = 10
+    while(i>5){
+        console.log(i);
+        i--
+    }
+}
+
+function draw(){
+
+   
+}
